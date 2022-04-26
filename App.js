8f970219ee3dafed70721e0fab80dbfaed7cf460
Nweke Chinedu Alex <nwekealexchinedu@gmail.com>
2022-04-26 14:22:40 +0100

    started working on the tab bar

--- App.js
+++ App.js
@@ -9,8 +9,12 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Navigator
+        screenOptions={{headerShown: false}}
+        initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Restaurant" component={Restaurant} />
+        <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
       </Stack.Navigator>
     </NavigationContainer>
   );
