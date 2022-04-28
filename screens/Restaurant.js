17c808b28659959ddea30f36bc3f6894f1c2e028
Nweke Chinedu Alex <nwekealexchinedu@gmail.com>
2022-04-28 09:10:52 +0100

    started working on the restaurant header component

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -13,11 +13,30 @@ import {isIphoneX} from 'react-native-iphone-x-helper';
 import {Icons, COLORS, SIZES, FONTS} from '../constants';
 
 const Restaurant = ({navigation, route}) => {
+  const [restaurant, setRestaurant] = React.useState(null);
+  const [currentLocation, setCurrentLocation] = React.useState(null);
+
+  React.useEffect(() => {
+    let {item, currentLocation} = route?.params;
+
+    setRestaurant(item);
+    setCurrentLocation(currentLocation);
+  }, [route?.params]);
+
+  function renderHeader() {
+    return <View />;
+  }
+
   return (
-    <View>
-      <Text>Restaurant</Text>
-    </View>
+    <SafeAreaView style={styles?.container}>{renderHeader()}</SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS?.lightGray2,
+  },
+});
 
 export default Restaurant;
