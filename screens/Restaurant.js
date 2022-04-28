5267c38c12e9a41800b12b6ace55d1a1ee6d2b34
Nweke Chinedu Alex <nwekealexchinedu@gmail.com>
2022-04-28 09:42:51 +0100

    finished working on the restaurant header component

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -10,7 +10,7 @@ import {
 } from 'react-native';
 import {isIphoneX} from 'react-native-iphone-x-helper';
 
-import {Icons, COLORS, SIZES, FONTS} from '../constants';
+import {Icons, COLORS, SIZES, FONTS, icons} from '../constants';
 
 const Restaurant = ({navigation, route}) => {
   const [restaurant, setRestaurant] = React.useState(null);
@@ -24,11 +24,48 @@ const Restaurant = ({navigation, route}) => {
   }, [route?.params]);
 
   function renderHeader() {
-    return <View />;
+    return (
+      <View style={{flexDirection: 'row', height: 50}}>
+        <TouchableOpacity
+          style={{
+            width: 50,
+            justifyContent: 'center',
+            paddingLeft: SIZES?.padding * 2,
+          }}
+          onPress={() => navigation.goBack()}>
+          <Image source={icons.back} style={styles?.headerImg} />
+        </TouchableOpacity>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <View
+            style={{
+              height: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+              paddingHorizontal: SIZES?.padding * 3,
+              borderRadius: SIZES?.radius,
+              backgroundColor: COLORS?.lightGray3,
+            }}>
+            <Text style={FONTS.h3}>{restaurant?.name}</Text>
+          </View>
+        </View>
+        <TouchableOpacity
+          style={{
+            width: 50,
+            justifyContent: 'center',
+            paddingRight: SIZES.padding * 2,
+          }}
+          onPress={() => {}}>
+          <Image source={icons.list} style={styles.headerImg} />
+        </TouchableOpacity>
+      </View>
+    );
   }
 
   return (
-    <SafeAreaView style={styles?.container}>{renderHeader()}</SafeAreaView>
+    <SafeAreaView style={styles?.container}>
+      {renderHeader()}
+      {/* <Text>header</Text> */}
+    </SafeAreaView>
   );
 };
 
@@ -36,6 +73,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS?.lightGray2,
+  },
+  headerImg: {
+    width: 30,
+    height: 30,
+    resizeMode: 'contain',
   },
 });
 
